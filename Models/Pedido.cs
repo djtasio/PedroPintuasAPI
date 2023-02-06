f4d7b8b508996441f72935554b7cacf420f4150c
Guillermo Sanchez Sanz <guillermomonterde@gmail.com>
2023-02-06 12:02:06 +0100

    Cambio en los modelos para la migracion

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -2,12 +2,15 @@ namespace API_PedroPinturas.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+//SI QUIERO QUE NO ME MAPEE [NotMapped]
 public class Pedido{
-    [Key]
+    [Key]// [Column("blog_id")]
     public int Id { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //PARA QUE INSERTE AL INSERTAR EL PRODUCTO
     public DateTime Fecha { get; set; }
     public Producto? Productos { get; set; }
     public bool Entrega24h { get; set; }
     public string? Direccion { get; set; }
+    [Column(TypeName = "decimal(5, 2)")]
     public double Precio { get; set; }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,13 +1,20 @@
 namespace API_PedroPinturas.Models;
+using Microsoft.EntityFrameworkCore; 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+[Index(nameof(User), IsUnique = true)]
 public class Usuario{
     [Key]
     public int Id { get; set; }
+    [Required]
     public string? User { get; set; }
+    [Required]
     public string? Contrasenia { get; set; }
+    [Required]
     public string? NombreApellidos { get; set; }
+    [Required]
+    [Range(0, 9)]
     public int Telefono { get; set; }
     public List<Pedido>? Pedidos{ get; set; }
 }
